c1a37c50081b00ce6502614e8b6d1af540556813
Orna <zannatulmawa62@gmail.com>
2021-04-23 15:52:58 -0400

    owl carousel js file added

---
+++ index.js
@@ -0,0 +1,11 @@
+/* $ indicates JQuery*/
+
+$(document).ready(function(){
+    // banner owl carousel
+
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots:true,
+        items:1
+    });
+
+});
